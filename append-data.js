d658a86ab7fae6fa91c5609e0211cee3e80a4c68
Dave Gumba <dave_gumba@Yahoo.com>
2017-01-03 04:01:37 -0500

    Append data success

--- append-data.js
+++ append-data.js
@@ -1,8 +1,20 @@
+var fs = require('fs');
 var datasets = ['arrohome-items.json', 'bestbuy-items.json', 'cardkingdom-items.json',
                 'dollhouse-items.json', 'oopc-items.json', 'ottycb-items.json',
                 'saatchiart-items.json'];
 
+var allData = [];
+
 for(var i = 0; i < datasets.length; i++) {
     var currentPackage = require('./' + datasets[i]);
-    console.log(currentPackage);
+    allData = allData.concat(currentPackage);
 }
+
+//console.log(allData.length);
+
+var allDataJSON = JSON.stringify(allData);
+
+fs.writeFile('items.json', allDataJSON, function(error) {
+    if (error) return error;
+    console.log("Writing file success");
+});
