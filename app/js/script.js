f3814aee0e7314312427aba17f66410a10a88d56
Dave Gumba <dave_gumba@Yahoo.com>
2017-01-08 21:58:24 -0500

    Displayed price name, item, and pic.

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,32 @@
 /*
-TODO: Display: random item pic, item name, item price, on both sides
+TODO: Fill width equally and fully initially...
+TODO: Center images
 TODO: Display buttons (higher, lower, same)
 TODO: Make those buttons work
 */
+var $priceText = $(".price-text");
 var $leftCol = $("#left-col");
 var $rightCol = $("#right-col");
-var $picDiv = $(".pic-div");
+var $leftPic = $("#left-pic");
+var $leftItemName = $("#left-item-name");
+var $leftItemPrice = $("#left-item-price");
+var $rightPic = $("#right-pic");
+var $rightItemName = $("#right-item-name");
+var $rightItemPrice = $("#right-item-price");
 var jsonUrl = "https://api.myjson.com/bins/1df8v3";
 
 function generateRandomNumber(length) {
-    var index = Math.floor(Math.random() * (length - 1));
-    return index;
+    var firstRandNum = Math.floor(Math.random() * (length - 1));
+    var secondRandNum = Math.floor(Math.random() * (length - 1));
+
+    while (firstRandNum === secondRandNum) {
+        secondRandNum = Math.floor(Math.random() * (length - 1));
+    }
+
+    return [firstRandNum, secondRandNum];
 }
 
-var gradientIndex = generateRandomNumber(gradients.length);
+var gradientIndex = generateRandomNumber(gradients.length)[0];
 
 $leftCol.css({"background-color" : gradients[gradientIndex][0]});
 $rightCol.css({"background-color" : gradients[gradientIndex][1]});
@@ -22,6 +35,7 @@ $.ajax({
     url: jsonUrl,
     type: 'GET',
     success: function(data) {
+        jsonData = data;
         update(null);
 
         // logic
@@ -36,7 +50,22 @@ var update = function(input) {
     if (input != null) {
         // game
     } else {
-        // you have so much to learn about jQuery...
+        // initialization
+        var index = generateRandomNumber(jsonData.length);
+        var [indexOne, indexTwo] = index;
+        var [productOne, productTwo] = [jsonData[indexOne], jsonData[indexTwo]];
+
+        console.log(productOne.src, productOne.name, productOne.price);
+
+
+        $leftPic.attr("src", productOne.src);
+        $leftItemName.text(productOne.name);
+        $leftItemPrice.text(productOne.price);
+
+        $rightPic.attr("src", productTwo.src);
+        $rightItemName.text(productTwo.name);
+        $rightItemPrice.text(productTwo.price);
+
 
     }
 
